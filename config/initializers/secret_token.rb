7732f8d6231b0b5d7ce8ceb5315b5947ff5710df
Mark Flanagan <mark.evan.flanagan@gmail.com>
2014-01-27 15:45:30 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Barbot2::Application.config.secret_key_base = '7a028307a9b997b9bd6cd91593da4974820996022591e47aeb90c0c3c315c3c4e81e2a7b9f806d8572c00978c80ed621f3b2f30996abcc12cb1fd766312b6543'
